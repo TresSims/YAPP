f3e33ae4378aea065279dd919b42bc208e530124
Tres Sims <tres.sims95@gmail.com>
2020-02-08 23:14:22 -0500

    By all accounts this should work, and yet here we are.

--- scripts/Mechanics.gd
+++ scripts/Mechanics.gd
@@ -1,11 +1,12 @@
 extends RigidBody
 
-const ACCEL = 30
-const JUMP_SPEED = 500
-
-const MAX_SPEED = 25
-
+const ACCEL = 60
+const MAX_SPEED = 15
+const JERK = 3
+const JUMP_SPEED = 750
+var DECAY = 0.95
 var vel = Vector3(0,0,0)
+var THRESHOLD = .05
 
 onready var DownRay = get_node("DownRay")
 
@@ -17,8 +18,15 @@ func _physics_process(delta):
 func process_input(delta):
 	if Input.is_action_pressed("ui_right"):
 		vel.x += 1
+	if Input.is_action_just_pressed("ui_right") and linear_velocity.x < 0:
+		vel.x *= JERK
+		print("Jerk Input")
+	
 	if Input.is_action_pressed("ui_left"):
 		vel.x -= 1
+	if Input.is_action_just_pressed("ui_left") and linear_velocity.x > 0:
+		vel.x *= JERK
+		print("Jerk Input")
 	
 	if Input.is_action_just_pressed("ui_up")  && on_ground():
 		vel.y += 1
@@ -35,10 +43,17 @@ func process_input(delta):
 
 func process_movement(delta):
 	add_central_force(vel)
-	print(linear_velocity)
+	if(linear_velocity.x > MAX_SPEED):
+		linear_velocity.x = MAX_SPEED
+	# print(linear_velocity)
 
 func process_decay(delta):
 	vel = Vector3(0, 0, 0)
+	if abs(linear_velocity.x) > THRESHOLD:
+		if not Input.is_action_pressed("ui_left") and not Input.is_action_pressed("ui_right"):
+			linear_velocity.x *= DECAY
+	else:
+		linear_velocity.x = 0
 
 func on_ground():
 	return DownRay.is_colliding()
